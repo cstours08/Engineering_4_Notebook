0b5e4ba46e91946fe3e373080859fc11fa0cbbdd
cstours08 <cstours08.cstours08@charlottesvilleschools.org>
2020-11-24 19:51:47 +0000

    making some code better

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -3,25 +3,13 @@
 
 import random
 
-x= "y"
+x = ("roll")
 
-while x == "y":
-print ("your role:")
-	no = random.randint(1,6)
+while x == "roll":
+	print ("your role:")
+	role = random.randint(1,6)
 
-	if no == 1:
-		print("1")
-	if no == 2:
-		print("2")
-	if no == 3:
-		print("3")
-	if no == 4:
-		print("4")
-	if no == 5:
-		print("5")
-	if no == 6:
-		print("6")
+	print (role)
 
-	x=input("press y to roll again and n to exit:")
+	x=input("type roll to roll again and hit enter to exit:")
 	print ("\n")
-
