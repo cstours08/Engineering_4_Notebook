288154a853d84fde01b0e4f1598529dff33b1bf7
cstours08 <cstours08.cstours08@charlottesvilleschools.org>
2020-11-30 17:48:59 +0000

    commented some code

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -1,15 +1,17 @@
+
 #Automatic Dice Roller
 # Written by Corin Saint Ours
 
 import random
 
-x = ("roll")
+x = ("roll") #this gives x a specific input to look for
 
-while x == "roll":
+while x == "roll": #this is what occurs when the correct input is found
 	print ("your role:")
-	role = random.randint(1,6)
+	role = random.randint(1,6) #this selects a random number
 
 	print (role)
 
-	x=input("type roll to roll again and hit enter to exit:")
+	x=input("type roll to roll again and hit enter to exit:") 
+	#this looks for the next input and gives instructions
 	print ("\n")
